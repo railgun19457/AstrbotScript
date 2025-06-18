a58825131d26133b84302c61eac05fe5b6de349e
railgun19457 <railgun19457@163.com>
2025-06-18 22:15:10 +0800

    修复redis启动问题

--- astrbot_napcat_wechatpadpro.sh
+++ astrbot_napcat_wechatpadpro.sh
@@ -1,3 +1,4 @@
+rm -rf ./WeChatPadPro/redis/data/*
 docker network create --driver bridge astrbot
 docker compose -f./astrbot_napcat.yml up -d
 docker compose -f./napcat/napcat.yml up -d
--- astrbot_wechatpadpro.sh
+++ astrbot_wechatpadpro.sh
@@ -1,3 +1,4 @@
+rm -rf ./WeChatPadPro/redis/data/*
 docker network create --driver bridge astrbot
 docker compose -f./astrbot.yml up -d
 docker compose -f./WeChatPadPro/wechat.yml up -d
